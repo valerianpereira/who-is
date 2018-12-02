771ae438d3341f7f675a0e8affa8851d95ef53e8
Darshan Gada <gadadarshan@gmail.com>
2018-12-02 21:02:48 +0530

    All CONSTANTS goes here, Need to implement dotenv

---
+++ api/config/constants.php
@@ -0,0 +1,18 @@
+<?php
+
+define('VERIFY_TOKEN','');
+
+// freedomainapi.com (One Request Per Minute)
+define('FREEDOMAIN_API_URL','http://api.freedomainapi.com/?');
+define('FREEDOMAIN_API_KEY','');
+define('FREEDOMAIN_API_METHOD','GET');
+define('FREEDOMAIN_API_STATUS','DISABLED');
+
+// jsonwhoisapi.com (1000 Requests Per Month)
+define('JSONWHOIS_API_ACC_NO','');
+define('JSONWHOIS_API_KEY','');
+define('JSONWHOIS_API_STATUS','ENABLED');
+
+// 
+
+?>
