51ef51b8e794551c1a1f0cbbaee4f11b3aa48b7b
Darshan Gada <gadadarshan@gmail.com>
2018-12-15 21:38:45 +0530

    WhoisXML Api Implemented

---
+++ api/sources/whoisxmlapi.php
@@ -0,0 +1,29 @@
+<?php 
+
+require_once(realpath(dirname(__FILE__).'/../config/constants.php'));
+require_once(realpath(dirname(__FILE__).'/../config/reusables.php'));
+
+// WHOISXMLAPI API
+// Refer : https://whoisapi.whoisxmlapi.com/docs
+$requiredFields = array(
+  'domain',
+  'Authorization'
+);
+
+validateRequest(array_merge(apache_request_headers(), $_POST), $requiredFields);
+
+// Incase if you want to enquire for domain availability just add one more key 
+// 'da' => 2
+$reqParams = array(
+  'apiKey' => WHOISXML_API_KEY,
+  'outputFormat' => 'JSON',
+  'thinWhois' => 1,
+  'domainName' => trim($_POST['domain'])
+);
+
+$respArr = array();
+$respArr = json_decode(curlIt(WHOISXML_API_URL, $reqParams, 'GET'));
+
+echoResponse($respArr);
+
+?>
